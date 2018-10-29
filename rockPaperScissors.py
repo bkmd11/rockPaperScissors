a4f7e3aa3eb902ef0103f72d431a2060671592e6
bkmd11 <bkendall11@gmail.com>
2018-10-29 19:27:44 -0400

    modified formating to more closely match rules

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -3,51 +3,48 @@
 
 import random
 
-def add(a,b):
-    a+b=c
-    return(c)
+maxGames=3
+RPS=['rock','paper','scissors']     
 
-RPS=['rock','paper','scissors']     #list of choices
-
-I=0   #computer score
-You=0  #player score
-game=0   #keeps track of game number
+computerScore=0   
+playerScore=0  
+gameCount=0   
 print('I challenge you to rock paper scissors, best 2 out of 3!')
-while game<=2:    #loops for 3 rounds
+while gameCount < maxGames:    #loops for 3 rounds
     print('rock paper scissors!')
     player=input()
     computer=random.choice(RPS)
     print(computer)
     if player==computer:
         print('its a tie!')
-        game-=1   #makes sure there can be no tie at the end
+        gameCount -= 1   #makes sure there can be no tie at the end
 
     #sets the rules of rock paper scissors and tracks wins    
-    elif player=='rock':
-        if computer=='paper':
+    elif player == 'rock':
+        if computer == 'paper':
             print('I win!')
-            I+=1
-        elif computer=='scissors':
+            computerScore += 1
+        elif computer == 'scissors':
             print('You win')
-            You+=1
-    elif player=='paper':
-        if computer=='rock':
+            playerScore += 1
+    elif player == 'paper':
+        if computer == 'rock':
             print('you win')
-            You+=1
-        elif computer=='scissors':
+            playerScore += 1
+        elif computer == 'scissors':
             print('I win!')
-            I+=1
-    elif player=='scissors':
-        if computer=='rock':
+            computerScore += 1
+    elif player == 'scissors':
+        if computer == 'rock':
             print('I win!')
-            I+=1
-        elif computer=='paper':
+            computerScore += 1
+        elif computer == 'paper':
             print('you win')
-            You+=1
+            playerScore += 1
             
-    game+=1
-print('I won '+str(I)+' games, and you won '+str(You)+' games')
-if I>You:
+    gameCount+=1
+print('I won {} games, and you won {} games'.format(computerScore, playerScore))
+if computerScore > playerScore:
     print('I beat you!')
 else:
     print('You are clearly very good at this game')
