31e11fd3be9880d9e2b3632e347486c38eb0e510
bkmd11 <bkendall11@gmail.com>
2019-01-08 20:34:31 -0500

    changed my class around more to make it actually have purpose

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,44 +1,73 @@
 #! python3
 #rock paper scissors
 
+""" Still need to play around with changing the game score
+with my fancy shmancy class thingy...
+Might not actually need that as a method though, works as is.
+Also need to re add try/except stuff to check for a valid choice
+And eventually break the game early in the event of 2 wins
+but the wife is getting all worked up.
+
+
+Oh and I should eliminate my use of 'choice' because its part of the random function
+and that causes confusion...
+
+plus read pep8 for formating
+"""
 import random
 
-class gameState():
-    score = 0
-    choice = ''
-    index = ''
+class rps_game():
     
-def gameLogic(player1Choice, player2Choice):
-    if player1Choice == player2Choice:
+    # Initilize the players
+    def __init__(self, score, choice, index):
+        self.score = score
+        self.choice = choice
+        self.index = index
+
+    # Method to change score
+    def score_method(self, game_outcome):
+        if self == winner:
+            score += 1
+
+    # Method for choice
+    def make_choice(self):
+        self.choice = input('rock, paper scissors! Make your choice... ')
+        return self.choice
+    
+    # Method for indexing
+    def choice_index(self, choice):
+        self.index = RPS.index(self.choice)
+        return self.index
+
+# Determines which player wins based on an index of their choice   
+def game_logic(player_1_choice, player_2_choice):
+    if player_1_choice == player_2_choice:
         return 0
 
-    playerNextIndex = (player1Choice + 1) % 3
+    player_next_index = (player_1_choice + 1) % 3
     
-    winner = 2 if playerNextIndex == player2Choice else 1
+    winner = 2 if player_next_index == player_2_choice else 1
     
     return winner
 
-
+# Setting global variables and initializing the players
 RPS = ['rock', 'paper', 'scissors']
-maxGames = 3
-player = gameState()
-computer = gameState()
-gameCount = 0
+max_games = 3
+player = rps_game(0, '', '')
+computer = rps_game(0, '', '')
+game_count = 0
 
+# Main body of game
 print('I challenge you to rock paper scissors, best 2 out of 3!')
 
-while gameCount < maxGames:    #loops for 3 rounds
-    player.choice = input('rock paper scissors! Make your choice:')
-    try:
-        player.index = RPS.index(player.choice)
-    except (ValueError):
-        print('Are you serious? The game is called "rock, paper, scissors"! Maybe try picking one of those, hmmmmmmm?')
-        continue
-
-    computer.index = random.randint(0, 2)
-    computer.choice = RPS[computer.index]
+while game_count < max_games:    #loops for 3 rounds
+    player.make_choice()
+    player.choice_index(player.choice)
+    computer.choice = random.choice(RPS)
+    computer.choice_index(computer.choice)
+    
     print(computer.choice)
-    winner = gameLogic(player.index, computer.index)
+    winner = game_logic(player.index, computer.index)
 
     if winner == 0:
         print('It\'s a tie!')
@@ -50,7 +79,7 @@ while gameCount < maxGames:    #loops for 3 rounds
         else:
             player.score += 1
             print('apparently you win this time')
-        gameCount += 1
+        game_count += 1
 
 print('I won {} games and you won {}'.format(computer.score, player.score))
 
